33c81f40e004ff5030f28bca405f4557f7a4fbbe
SulthanRaghib <raghib.smp@gmail.com>
2026-01-09 09:03:34 +0700

    feat: update JobPositionSeeder with new job positions and IDs

--- database/seeders/JobPositionSeeder.php
+++ database/seeders/JobPositionSeeder.php
@@ -13,19 +13,34 @@ class JobPositionSeeder extends Seeder
     public function run(): void
     {
         $positions = [
-            ['name' => 'Kepala BAPETEN', 'grade' => 17],
-            ['name' => 'Sekretaris Utama', 'grade' => 16],
-            ['name' => 'Deputi Perizinan dan Inspeksi', 'grade' => 16],
-            ['name' => 'Deputi Pengkajian Keselamatan Nuklir', 'grade' => 16],
-            ['name' => 'Direktur Pengaturan Pengawasan Instalasi dan Bahan Nuklir', 'grade' => 15],
-            ['name' => 'Analis Kepegawaian Ahli Muda', 'grade' => 10],
-            ['name' => 'Pranata Komputer Ahli Pertama', 'grade' => 9],
-            ['name' => 'Arsiparis Mahir', 'grade' => 8],
-            ['name' => 'Pengadministrasi Umum', 'grade' => 5],
+            ['id' => 1, 'name' => 'Kepala (Es-1)'],
+            ['id' => 2, 'name' => 'Deputi (Es-1)'],
+            ['id' => 3, 'name' => 'Sekretaris Utama (Es-1)'],
+            ['id' => 4, 'name' => 'Direktur (Es-2)'],
+            ['id' => 5, 'name' => 'Kepala Pusat (Es-2)'],
+            ['id' => 6, 'name' => 'Kepala Inspektorat (Es-2)'],
+            ['id' => 7, 'name' => 'Kepala Biro (Es-2)'],
+            ['id' => 8, 'name' => 'Kepala Subdirektorat (Es-3)'],
+            ['id' => 9, 'name' => 'Kepala Bagian (Es-3)'],
+            ['id' => 10, 'name' => 'Kepala Bidang (Es-3)'],
+            ['id' => 11, 'name' => 'Kepala Balai (Es-3)'],
+            ['id' => 12, 'name' => 'Kepala Subbagian (Es-4)'],
+            ['id' => 13, 'name' => 'Pengelola Kegiatan (Es-3)'],
+            ['id' => 14, 'name' => 'Sub Koordinator (Es-4)'],
+            ['id' => 15, 'name' => 'Kepala Seksi (Es-4)'],
+            ['id' => 16, 'name' => 'Kepala Kelompok (Es-3)'],
+            ['id' => 17, 'name' => 'Sekretaris (Es-99)'],
+            ['id' => 99, 'name' => 'Staf (Es-99)'],
+            // ['id' => 0, 'name' => '- (Es-99)'], // Skipping ID 0 as it often causes issues with auto-increment
         ];
 
+        JobPosition::unguard();
         foreach ($positions as $position) {
-            JobPosition::firstOrCreate(['name' => $position['name']], $position);
+            JobPosition::updateOrCreate(
+                ['id' => $position['id']],
+                ['name' => $position['name']]
+            );
         }
+        JobPosition::reguard();
     }
 }
