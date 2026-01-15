cdfd49c6399eabc5498bc466c2c351e03b198876
SulthanRaghib <raghib.smp@gmail.com>
2026-01-15 15:07:54 +0700

    feat: add HobbiesRelationManager for managing user hobbies

---
+++ app/Filament/Resources/Users/RelationManagers/HobbiesRelationManager.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Filament\Resources\Users\RelationManagers;
+
+use Filament\Actions\AssociateAction;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\CreateAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DissociateAction;
+use Filament\Actions\DissociateBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class HobbiesRelationManager extends RelationManager
+{
+    protected static string $relationship = 'hobbies';
+
+    protected static ?string $title = 'Hobi';
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->label('Nama Hobi')
+                    ->required()
+                    ->maxLength(255),
+                Textarea::make('notes')
+                    ->label('Keterangan'),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                TextColumn::make('name')->label('Hobi'),
+                TextColumn::make('notes')->label('Keterangan')->limit(50),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                CreateAction::make(),
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
